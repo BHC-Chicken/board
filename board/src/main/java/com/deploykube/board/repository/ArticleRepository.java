041b72f8338dc1011acd6410aa5175145dcd5b27
Park <tij05078@gmail.com>
2022-08-09 02:35:03 +0900

    @RepositoryRestResource 추가

--- board/src/main/java/com/deploykube/board/repository/ArticleRepository.java
+++ board/src/main/java/com/deploykube/board/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.deploykube.board.repository;
 
 import com.deploykube.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long>{
 
 }
