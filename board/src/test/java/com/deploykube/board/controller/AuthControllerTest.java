b9d7618308a5bed1e5a3fb54ae4765c8dabb8d70
Park <tij05078@gmail.com>
2022-08-13 01:58:55 +0900

    login page test 추가

---
+++ board/src/test/java/com/deploykube/board/controller/AuthControllerTest.java
@@ -0,0 +1,35 @@
+package com.deploykube.board.controller;
+
+import com.deploykube.board.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("view 컨트롤러 - 인증")
+@WebMvcTest
+@Import(SecurityConfig.class)
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view] [GET] 로그인 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenTryingToLogin_thenReturnsLogInView() throws Exception {
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+}
