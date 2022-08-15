fe5bbb8dc06651d801075fde895444aa52846ff7
Park <tij05078@gmail.com>
2022-08-16 03:31:17 +0900

    article controller 구현 및 테스트 추가

--- board/src/main/java/com/deploykube/board/controller/ArticleController.java
+++ board/src/main/java/com/deploykube/board/controller/ArticleController.java
@@ -1,27 +1,45 @@
 package com.deploykube.board.controller;
 
+import com.deploykube.board.Response.ArticleResponse;
+import com.deploykube.board.Response.ArticleWithCommentsResponse;
+import com.deploykube.board.domain.type.SearchType;
+import com.deploykube.board.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 20,sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable)
+                .map(ArticleResponse::from));
+
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(ModelMap map, @PathVariable Long articleId) {
-        map.addAttribute("article", "article"); // TODO : 구현할 때 실제 데이터르 넣어줘야 함
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article =  ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentResponses());
         return "articles/detail";
     }
 
--- board/src/test/java/com/deploykube/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/deploykube/board/controller/ArticleControllerTest.java
@@ -1,24 +1,40 @@
 package com.deploykube.board.controller;
 
 import com.deploykube.board.config.SecurityConfig;
+import com.deploykube.board.dto.ArticleWithCommentsDto;
+import com.deploykube.board.dto.UserAccountDto;
+import com.deploykube.board.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @DisplayName("view 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)
 @Import(SecurityConfig.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
+
+    @MockBean
+    private ArticleService articleService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -27,21 +43,33 @@ class ArticleControllerTest {
     @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+
+        given(articleService.searchArticles(ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), ArgumentMatchers.any(Pageable.class))).willReturn(Page.empty());
+
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+
+        then(articleService).should().searchArticles(ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), ArgumentMatchers.any(Pageable.class));
     }
 
     @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
-        mvc.perform(get("/articles/1"))
+
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
+
+        mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
+
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled
@@ -63,4 +91,35 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles/search-hashtag"));
     }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#hashtag",
+                LocalDateTime.now(),
+                "park",
+                LocalDateTime.now(),
+                "park"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                1L,
+                "park",
+                "test1234",
+                "test1234@sample.com",
+                "park",
+                "park",
+                LocalDateTime.now(),
+                "park",
+                LocalDateTime.now(),
+                "park"
+        );
+    }
+
 }
